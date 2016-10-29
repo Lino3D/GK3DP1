296999723c00be6be403ffebdb8a5c23eee4717e
Lino3D <kamiskijakub@gmail.com>
2016-10-29 13:54:43 +0200

    Commit with stuff so far.

---
+++ GK3DP1/GK3DP1/Cube.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GK3DP1
+{
+    public class Cube
+    {
+        public float Width;
+        public float Height;
+        public float Depth;
+
+        public Vector3 Position ;
+
+        // public float  = 40;
+
+       
+        public VertexPositionNormalTexture[] vertexes;
+
+        public Cube(float x, float y, float z)
+        {
+            Width = x;
+            Height = y;
+            Depth = z;
+            Position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        public Cube(float x, float y, float z, Vector3 startingPosition)
+        {
+            Width = x;
+            Height = y;
+            Depth = z;
+            Position = startingPosition;
+        }
+
+
+        public VertexPositionNormalTexture[]
+                                   MakeCube()
+        {
+            vertexes = new VertexPositionNormalTexture[36];
+
+            //top face
+            Vector3 topLeftFront = Position + new Vector3(-Width, 1.0f, -Depth);
+            Vector3 topLeftBack = Position + new Vector3(-Width, 1.0f, Depth);
+            Vector3 topRightFront = Position + new Vector3(Width, 1.0f, -Depth);
+            Vector3 topRightBack = Position + new Vector3(Width, 1.0f, Depth);
+
+            // Calculate the position of the vertices on the bottom face.
+            Vector3 btmLeftFront = Position + new Vector3(-Width, -Height, -Depth);
+            Vector3 btmLeftBack = Position + new Vector3(-Width, -Height, Depth);
+            Vector3 btmRightFront = Position + new Vector3(Width, -Height, -Depth);
+            Vector3 btmRightBack = Position +  new Vector3(Width, -Height, Depth);
+
+            // Normal vectors for each face (needed for lighting / display)
+            Vector3 normalFront = new Vector3(0.0f*Height, 0.0f*Width, 1.0f* Depth);
+            Vector3 normalBack = new Vector3(0.0f * Height, 0.0f * Width, -1.0f * Depth);
+            Vector3 normalTop = new Vector3(0.0f * Height, 1.0f*Width, 0.0f * Depth);
+            Vector3 normalBottom = new Vector3(0.0f * Height, -1.0f * Width, 0.0f * Depth);
+            Vector3 normalLeft = new Vector3(-1.0f * Height, 0.0f * Width, 0.0f * Depth);
+            Vector3 normalRight = new Vector3(1.0f * Height, 0.0f * Width, 0.0f * Depth);
+
+            // UV texture coordinates
+            Vector2 textureTopLeft = new Vector2(1.0f*Height, 0.0f*Width);
+            Vector2 textureTopRight = new Vector2(0.0f * Height, 0.0f * Width);
+            Vector2 textureBottomLeft = new Vector2(1.0f * Height, 1.0f * Width);
+            Vector2 textureBottomRight = new Vector2(0.0f * Height, 1.0f * Width);
+
+            // Add the vertices for the FRONT face.
+            vertexes[0] = new VertexPositionNormalTexture(topLeftFront, normalFront, textureTopLeft);
+            vertexes[1] = new VertexPositionNormalTexture(btmLeftFront, normalFront, textureBottomLeft);
+            vertexes[2] = new VertexPositionNormalTexture(topRightFront, normalFront, textureTopRight);
+            vertexes[3] = new VertexPositionNormalTexture(btmLeftFront, normalFront, textureBottomLeft);
+            vertexes[4] = new VertexPositionNormalTexture(btmRightFront, normalFront, textureBottomRight);
+            vertexes[5] = new VertexPositionNormalTexture(topRightFront, normalFront, textureTopRight);
+
+            // Add the vertices for the BACK face.
+            vertexes[6] = new VertexPositionNormalTexture(topLeftBack, normalBack, textureTopRight);
+            vertexes[7] = new VertexPositionNormalTexture(topRightBack, normalBack, textureTopLeft);
+            vertexes[8] = new VertexPositionNormalTexture(btmLeftBack, normalBack, textureBottomRight);
+            vertexes[9] = new VertexPositionNormalTexture(btmLeftBack, normalBack, textureBottomRight);
+            vertexes[10] = new VertexPositionNormalTexture(topRightBack, normalBack, textureTopLeft);
+            vertexes[11] = new VertexPositionNormalTexture(btmRightBack, normalBack, textureBottomLeft);
+
+            // Add the vertices for the TOP face.
+            vertexes[12] = new VertexPositionNormalTexture(topLeftFront, normalTop, textureBottomLeft);
+            vertexes[13] = new VertexPositionNormalTexture(topRightBack, normalTop, textureTopRight);
+            vertexes[14] = new VertexPositionNormalTexture(topLeftBack, normalTop, textureTopLeft);
+            vertexes[15] = new VertexPositionNormalTexture(topLeftFront, normalTop, textureBottomLeft);
+            vertexes[16] = new VertexPositionNormalTexture(topRightFront, normalTop, textureBottomRight);
+            vertexes[17] = new VertexPositionNormalTexture(topRightBack, normalTop, textureTopRight);
+
+            // Add the vertices for the BOTTOM face.
+            vertexes[18] = new VertexPositionNormalTexture(btmLeftFront, normalBottom, textureTopLeft);
+            vertexes[19] = new VertexPositionNormalTexture(btmLeftBack, normalBottom, textureBottomLeft);
+            vertexes[20] = new VertexPositionNormalTexture(btmRightBack, normalBottom, textureBottomRight);
+            vertexes[21] = new VertexPositionNormalTexture(btmLeftFront, normalBottom, textureTopLeft);
+            vertexes[22] = new VertexPositionNormalTexture(btmRightBack, normalBottom, textureBottomRight);
+            vertexes[23] = new VertexPositionNormalTexture(btmRightFront, normalBottom, textureTopRight);
+
+            // Add the vertices for the LEFT face.
+            vertexes[24] = new VertexPositionNormalTexture(topLeftFront, normalLeft, textureTopRight);
+            vertexes[25] = new VertexPositionNormalTexture(btmLeftBack, normalLeft, textureBottomLeft);
+            vertexes[26] = new VertexPositionNormalTexture(btmLeftFront, normalLeft, textureBottomRight);
+            vertexes[27] = new VertexPositionNormalTexture(topLeftBack, normalLeft, textureTopLeft);
+            vertexes[28] = new VertexPositionNormalTexture(btmLeftBack, normalLeft, textureBottomLeft);
+            vertexes[29] = new VertexPositionNormalTexture(topLeftFront, normalLeft, textureTopRight);
+
+            // Add the vertices for the RIGHT face.
+            vertexes[30] = new VertexPositionNormalTexture(topRightFront, normalRight, textureTopLeft);
+            vertexes[31] = new VertexPositionNormalTexture(btmRightFront, normalRight, textureBottomLeft);
+            vertexes[32] = new VertexPositionNormalTexture(btmRightBack, normalRight, textureBottomRight);
+            vertexes[33] = new VertexPositionNormalTexture(topRightBack, normalRight, textureTopRight);
+            vertexes[34] = new VertexPositionNormalTexture(topRightFront, normalRight, textureTopLeft);
+            vertexes[35] = new VertexPositionNormalTexture(btmRightBack, normalRight, textureBottomRight);
+
+            return vertexes;
+        }
+    }
+}
---
+++ GK3DP1/GK3DP1/Game1.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GK3DP1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private Vector3 camPosition;
+        private Vector3 camTarget;
+        private Matrix projectionMatrix;
+        private Matrix viewMatrix;
+        private Matrix worldMatrix;
+
+        //BasicEffect for rendering
+        private BasicEffect basicEffect;
+
+        //Geometric info
+       // private VertexBuffer vertexBuffer;
+
+        private VertexPositionNormalTexture[] Station;
+        private VertexPositionNormalTexture[] railWay;
+
+        private bool orbit = false;
+
+        Texture2D checkerBoardTexture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        ///
+
+        protected override void Initialize()
+        {
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                               MathHelper.ToRadians(45f),
+                               GraphicsDevice.DisplayMode.AspectRatio,
+                1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         new Vector3(0f, 1f, 0f));// Y up
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.
+                          Forward, Vector3.Up);
+
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.AmbientLightColor = Vector3.One;
+            basicEffect.DirectionalLight0.Enabled = true;
+            basicEffect.DirectionalLight0.DiffuseColor =
+                                        Vector3.One;
+            basicEffect.DirectionalLight0.Direction =
+                     Vector3.Normalize(Vector3.One);
+            basicEffect.LightingEnabled = true;
+            basicEffect.TextureEnabled = true;
+            basicEffect.Texture = checkerBoardTexture;
+
+            //vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(
+            //               VertexPositionColor), vertices.Length, BufferUsage.
+            //               WriteOnly);
+
+            //vertexBuffer.SetData<VertexPositionColor>(vertices);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            var myCube = new Cube(20,40,60);
+            Station = myCube.MakeCube();
+            var mySecondCube = new Cube(5, 5, 50, new Vector3(0.0f,-30.0f,1.0f));
+            railWay = mySecondCube.MakeCube();
+            using (var stream = TitleContainer.OpenStream("Textures/tex.jpg"))
+            {
+                checkerBoardTexture = Texture2D.FromStream(this.GraphicsDevice, stream);
+            }
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        ///
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+              //  camPosition.X -= 1f;
+                camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+             //   camPosition.X += 1f;
+                camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+               // camPosition.Y -= 1f;
+                camTarget.Y += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+              //  camPosition.Y += 1f;
+                camTarget.Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                camPosition.Z -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                camPosition.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                camPosition.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                orbit = !orbit;
+            }
+
+            if (orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(1f));
+                camPosition = Vector3.Transform(camPosition,
+                              rotationMatrix);
+            }
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         Vector3.Up);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            // GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+            basicEffect.TextureEnabled = true;
+            basicEffect.Texture = checkerBoardTexture;
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+           // GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.
+                    Passes)
+            {
+                pass.Apply();
+                graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList, Station,
+                                 0, 12);
+                graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList, railWay,
+                               0, 12);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
